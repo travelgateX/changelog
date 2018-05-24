3dc98b8aaa1f02625cb63d3c0c3367b5af9cd27f
Carlos Guzmán <cguzman@xmltravelgate.com>
2018-05-24 17:08:10 +0200

    Add more handlers, log transactions

--- server.go
+++ server.go
@@ -20,12 +20,14 @@ func main() {
 	log.SetLevel(config.DebugMode)
 	db, err = context.OpenDB(config)
 
-	// AutoMigrate : Check its working ok!! <=================
+	// MigrateFullDB : Check its working ok!! <=================
 	context.MigrateFullDB(db)
 
 	// Config url routes
 	router := mux.NewRouter()
 	router.HandleFunc("/changes", GetChanges).Methods("GET")
+	router.HandleFunc("/change/{id}", GetChange).Methods("GET")
+	router.HandleFunc("/change", CreateChange).Methods("POST")
 
 	// Start HTTP server
 	log.Info("== Changelog server ON == Listening at port:%s ==", config.HTTPPort)
@@ -35,10 +37,39 @@ func main() {
 
 }
 
-// GetChanges : All changes handler
+// GetChanges : [GET] Changes List
 func GetChanges(w http.ResponseWriter, r *http.Request) {
-	log.Info("[API REQUEST] [%s]", tools.Path(r))
+	logTransaction(r)
+
 	var changes []context.Change
 	db.Find(&changes)
 	json.NewEncoder(w).Encode(&changes)
+}
+
+// GetChange : [GET] Change by ID
+func GetChange(w http.ResponseWriter, r *http.Request) {
+	logTransaction(r)
+
+	params := mux.Vars(r)
+	var change context.Change
+	db.First(&change, params["id"])
+	json.NewEncoder(w).Encode(&change)
+}
+
+// CreateChange : [POST] Create Change
+func CreateChange(w http.ResponseWriter, r *http.Request) {
+	logTransaction(r)
+
+	var change context.Change
+	json.NewDecoder(r.Body).Decode(&change)
+	db.Create(&change)
+	json.NewEncoder(w).Encode(&change)
+}
+
+// Log transaction with params
+func logTransaction(r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Error("Parse form has exploited: %s", err)
+	}
+	log.Info("[API REQUEST] [%s] [%s]", tools.Path(r), r.Form)
 }
