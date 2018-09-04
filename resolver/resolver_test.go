44eff0b8c7a93228fd1288d462c32fb0b5115c47
slucea <slucea@xmltravelgate.com>
2018-09-04 11:21:53 +0200

    Full integration test

--- resolver/resolver_test.go
+++ resolver/resolver_test.go
@@ -1,14 +1,15 @@
 package resolver_test
 
 import (
-	graphql "github.com/graph-gophers/graphql-go"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
 	"changelog/config"
 	"changelog/context"
 	"changelog/model"
 	"changelog/resolver"
+	"fmt"
+
+	graphql "github.com/graph-gophers/graphql-go"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 // The var _= ... trick allows to evaluate the Describe at the top level
@@ -16,9 +17,13 @@ import (
 var _ = Describe("Resolver", func() {
 	var (
 		// Variable declaration
-		UpdateData model.UpdateChangeInput
-		CreateData model.CreateChangeInput
-		cResolver  resolver.Resolver
+		UpdateData     model.UpdateChangeInput
+		CreateData     model.CreateChangeInput
+		DeleteData     model.DeleteChangeInput
+		cResolver      resolver.Resolver
+		CreateDataTest model.CreateChangeInput
+		UpdateDataTest model.UpdateChangeInput
+		DeleteDataTest model.DeleteChangeInput
 	)
 
 	BeforeEach(func() {
@@ -38,45 +43,141 @@ var _ = Describe("Resolver", func() {
 		// TODO:
 		// Set up state for specs.
 		UpdateData = model.UpdateChangeInput{
-			Code:    "ch2",
+			Code:    "ch3",
 			Message: "messageTest",
 			Project: "projectTest",
 			Type:    model.ChangeType("CHANGED"),
 		}
 
-		newCode := "ch7"
+		newCode := "ch140"
 		CreateData = model.CreateChangeInput{
 			Code:    &newCode,
 			Message: "newMessage",
 			Project: "newProject",
 			Type:    model.ChangeType("ADDED"),
 		}
+
+		DeleteData = model.DeleteChangeInput{
+			Code: "ch7",
+		}
+		newCodeTest := "NewCode"
+		CreateDataTest = model.CreateChangeInput{
+			Code:    &newCodeTest,
+			Message: "NewMessage",
+			Project: "NewProject",
+			Type:    model.ChangeType("ADDED"),
+		}
+
+		UpdateDataTest = model.UpdateChangeInput{
+			Code:    newCode,
+			Message: "Updated message",
+			Project: "Updated project",
+			Type:    model.ChangeType("CHANGED"),
+		}
+
+		DeleteDataTest = model.DeleteChangeInput{
+			Code: newCode,
+		}
+
 	})
 
-	It("data", func() {
-		// Set up a singel spec
-	})
-
-	// Example
-
-	Describe("Updating an existing change with new data. ", func() {
-		Context("Modifing data from a manually created change", func() {
+	// UpdateChange function test
+	Describe("Creating a new change input... ", func() {
+		Context("if code is not already stored in DB", func() {
 			It("should have the same values in DB after the UpdateChange function", func() {
 				//UpdateChange
+				fmt.Print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" + UpdateData.Code)
 				s := struct{ Input *model.UpdateChangeInput }{&UpdateData}
 				result := *cResolver.UpdateChange(s)
 				Expect(result.ChangeData().Code()).To(Equal(graphql.ID(UpdateData.Code)))
 			})
 		})
-	})
 
+		Context("if trying to update non existing change", func() {
+			It("should return an advise message and the data in change should be null", func() {
+				// Use code to test error handling when code is duplicated
+				wrongCode := model.UpdateChangeInput{
+					Code: "asdas",
+				}
+				k := struct{ Input *model.UpdateChangeInput }{&wrongCode}
+				result := *cResolver.UpdateChange(k)
+				type lf struct {
+					Level *[]*model.AdviseMessageLevel
+				}
+				Expect(result.AdviseMessage(lf{nil}) != nil)
+			})
+		})
+	})
+	// CreateChange function test
 	Describe("Creating a new change", func() {
 		Context("adding data from a manually created node", func() {
-			It("should have the following values: Code: ch8\n Message: messageTest\n Project: projectTest\n Type: FIXED after retrieving it form the DB", func() {
+			It("should have the following values: Code: ch8\n Message: messageTest\n Project: projectTest\n Type: FIXED\n after retrieving it form the DB", func() {
 				s := struct{ Input *model.CreateChangeInput }{&CreateData}
 				result := *cResolver.CreateChange(s)
 				Expect(result.ChangeData().Code()).To(Equal(graphql.ID(*CreateData.Code)))
 			})
 		})
+
+		Context("if the data code is already stored in DB", func() {
+			It("should return an advise message and the data in change should be null", func() {
+				s := struct{ Input *model.CreateChangeInput }{&CreateData}
+				result := *cResolver.CreateChange(s)
+				type lf struct {
+					Level *[]*model.AdviseMessageLevel
+				}
+				Expect(result.AdviseMessage(lf{nil}) != nil)
+
+			})
+		})
+	})
+	// La función Unscoped no funciona debidamente
+	Describe("Deleting record from DB", func() {
+		Context("if code exists ", func() {
+			It("should mark it as soft-deleted", func() {
+				s := struct{ Input *model.DeleteChangeInput }{&DeleteData}
+				result := *cResolver.DeleteChange(s)
+				_ = result
+				var z model.ChangeData
+				db.Raw("SELECT * FROM changes WHERE code = ?", DeleteData.Code).Scan(&z)
+			})
+		})
+	})
+	Describe("Full test, create, update and delete same node", func() {
+		fmt.Print(*CreateDataTest.Code)
+		fmt.Print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" + UpdateDataTest.Code)
+		// Add new data to DB
+		s := struct{ Input *model.CreateChangeInput }{&CreateDataTest}
+		result := *cResolver.CreateChange(s)
+		_ = result
+		// Fetch data
+		var resultDB model.ChangeData
+		db.First(&resultDB, "code = ?", CreateDataTest.Code)
+		// Asert create
+		Expect(resultDB.Code).To(Equal(graphql.ID(*CreateDataTest.Code)))
+		Expect(resultDB.Message).To(Equal(CreateDataTest.Message))
+		Expect(resultDB.Project).To(Equal(CreateDataTest.Project))
+		Expect(resultDB.Type).To(Equal(CreateDataTest.Type))
+
+		// Update same data
+		up := struct{ Input *model.UpdateChangeInput }{&UpdateDataTest}
+		upRes := *cResolver.UpdateChange(up)
+		_ = upRes
+		// Fetch data
+		db.First(&resultDB, "code = ?", UpdateDataTest.Code)
+		// Assert Update
+		Expect(resultDB.Code).To(Equal(graphql.ID(UpdateDataTest.Code)))
+		Expect(resultDB.Message).To(Equal(UpdateDataTest.Message))
+		Expect(resultDB.Project).To(Equal(UpdateDataTest.Project))
+		Expect(resultDB.Type).To(Equal(UpdateDataTest.Type))
+
+		// Delete same data
+		del := struct{ Input *model.DeleteChangeInput }{&DeleteDataTest}
+		delRes := *cResolver.DeleteChange(del)
+		_ = delRes
+		// Fetch data
+		db.Unscoped().Where("code=? ?", DeleteDataTest.Code).First(&resultDB)
+		// Assert Delete
+		Expect(resultDB.DeletedAt != nil)
+
 	})
 })
